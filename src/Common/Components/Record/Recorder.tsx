0013b47c7f587e1dd7b5f5ef19b7204a31476fd8
Gennadij <gpolyakov77@gmail.com>
2023-09-10 11:06:59 +0400

    feat: add recorder component

---
+++ src/Common/Components/Record/Recorder.tsx
@@ -0,0 +1,152 @@
+
+import { Button } from "@mui/material";
+import React, { useRef, useState } from "react";
+import { useReactMediaRecorder } from "react-media-recorder";
+
+
+
+const Recorder2 = () => {
+    const videoRef: any = useRef();
+    const [link, setLink] = useState();
+    let arr: any = []
+
+    const {
+
+        mediaBlobUrl,
+
+        pauseRecording,
+
+        clearBlobUrl,
+
+        resumeRecording,
+
+        startRecording,
+
+        status,
+
+        stopRecording,
+
+    } = useReactMediaRecorder({ video: true, audio: true });
+
+
+    const handleVideo = async () => {
+
+        let blob = await fetch(mediaBlobUrl!)
+            .then(r => r.blob());
+            debugger
+
+        var reader = new FileReader();
+
+        reader.readAsDataURL(blob);
+
+        reader.onloadend = function () {
+
+            var base64data = reader.result;
+
+            handleUpload(base64data);
+
+        }
+    }
+
+    async function handleUpload(video: any) {
+        console.log('upload video', video);
+        try {
+            fetch("/api/upload", {
+                method: "POST",
+                body: JSON.stringify({ data: video }),
+                headers: { "Content-Type": "application/json" },
+            })
+                .then((response) => {
+                    console.log("response", response.status)
+                    response.json()
+                        .then((data) => {
+                            arr.push(data)
+                            setLink(arr[0].data)
+                            
+                        });
+                })
+                .catch( (err) =>
+                    console.log("err", err)
+                )
+        } catch (error) {
+            console.error("catch error", error);
+        }
+    }
+    return (
+        <div>
+
+            <div className='status'>
+                Статус: {status}
+                <br /><br />
+                Видео ссылка: {link || "нет сссылки..."}
+            </div>
+            <div>
+                <video
+                    ref={videoRef}
+                    src={mediaBlobUrl}
+                    controls
+                    autoPlay
+                    loop
+                />
+            </div>
+            <div className='buttons'>
+                {(status === "idle" || status === "recorder_error") && (
+                    <Button onClick={startRecording} variant='contained' color='primary' >Начать запись</Button>
+                )}
+
+                {(status === "recording" || status === "paused") && (
+                    <>
+
+                        <Button color='secondary' variant='contained' onClick={stopRecording}>Остановить запись</Button>
+                        {'    '}
+                        <Button
+                            color='primary'
+                            variant='contained'
+                            onClick={() => status === "paused" ? resumeRecording() : pauseRecording()}
+                        >
+                            {status === "paused" ? "Продолжить запись" : "Пауза"}
+                        </Button>
+                    </>
+
+                )}
+
+
+                {status === "stopped" && (
+                    <Button
+                        onClick={() => {
+                            resumeRecording();
+                            videoRef.current.load();
+                        }}
+                        variant='contained' color='primary'
+                    >
+                        Resume recording
+                    </Button>
+
+                )}{' '}
+
+                {status === "stopped" && (
+                    <Button
+                        onClick={handleVideo}
+                        variant='contained'
+                        color='primary'
+                    >
+                        Upload Recording/ handleVideo
+                    </Button>
+
+                )} {' '}
+
+                <Button
+                    onClick={clearBlobUrl}
+                    variant='outlined'
+                    color='primary'
+                >
+                    Сбросить видео
+                </Button>
+
+            </div>
+        </div>
+    )
+
+}
+
+export default Recorder2
--- src/pages/recorder/index.tsx
+++ src/pages/recorder/index.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/Common/Components/Button/Button"
 import { getLayout } from "@/Common/Components/Layout/BaseLayout/BaseLayout";
+import Recorder2 from "@/Common/Components/Record/Recorder";
 import { ThemeButton } from "@/Common/Enum/themeButton"
 import { colorsTheme } from "@/styles/StyledComponents/Common/colors";
 import { useState } from "react";
@@ -13,72 +14,12 @@ import { styled } from 'styled-components';
 
 const Recorder = () => {
 
-   
-    let deviceRecorder: any
-
-    async function startRecording(){
-        var stream =  await navigator.mediaDevices.getDisplayMedia({video: true, audio: true})
-    
-        deviceRecorder = new deviceRecorder(stream, {mimeType: "video/webm"});
-        deviceRecorder.ondataavailable = (e: any) => {
-            if(e.data.size > 0){
-                 chunks.push(e.data);
-                 console.log(e.data)
-            }
-        }
-        deviceRecorder.onstop = () => {
-            chunks = [];
-        }
-        deviceRecorder.start(250)
-    }
-    console.log(deviceRecorder)
-    // console.log(deviceRecorder.isTypeSupported("video/webm"))
-    // console.log(deviceRecorder.isTypeSupported("video/mp4"))
-    // console.log(deviceRecorder.isTypeSupported("video/mp4;codecs=avc1"))
-
-
-    function stopRecording() {
-        var filename = window.prompt("File name", "video"); // Ask the file name
-
-        deviceRecorder.stop(); // Stopping the recording
-        blob = new Blob(chunks, { type: "video/webm" })
-        chunks = [] // Resetting the data chunks
-        var dataDownloadUrl = URL.createObjectURL(blob);
-
-        // Downloadin it onto the user's device
-        let a = document.createElement('a')
-        a.href = dataDownloadUrl;
-        a.download = `${filename}.webm`
-        a.click()
-
-        URL.revokeObjectURL(dataDownloadUrl)
-    }
-    const [butonText, setButonText] = useState('Start recording')
-    let RECORDING_ONGOING = false;
-    const handler = () => {
-
-        RECORDING_ONGOING = !RECORDING_ONGOING; // Start / Stop recording
-        if (RECORDING_ONGOING) {
-            setButonText("Stop Recording")
-            startRecording(); // Start the recording
-        } else {
-            setButonText("Start Recording")
-            stopRecording(); // Stop screen recording
-        }
-    }
-
-    return (
+   return (
         <RecorderWrapper>
-            <Title>
-                Screen recorder
-            </Title>
-            <Button
-                theme={ThemeButton.PRIMARY}
-                type="button"
-                onClick={handler}
-            >
-                {butonText}
-            </Button>
+            <Title> Screen recorder  </Title>
+
+            <Recorder2/>
+          
         </RecorderWrapper>
 
     )
@@ -88,24 +29,23 @@ const Recorder = () => {
 Recorder.getLayout = getLayout
 export default Recorder
 
-const Title = styled.div`
-    width: 100%;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    font-size: 16px;
-    font-weight: 700;
-`
-
 const RecorderWrapper = styled.div`
     display: inline-flex;
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
-    width: 600px;
-    height: 200px;
-    background: ${colorsTheme.colors.dark[300]}
-
-
+    width: auto;
+    height: auto;
+    /* background: ${colorsTheme.colors.dark[300]} */
 `
+
+const Title = styled.div`
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    font-size: 18px;
+    font-weight: 700;
+`
+
 
